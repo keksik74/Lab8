3cae94c6927993ce5cb3bc93521574ec7ce1c9a9
Domanova.Maria <domanova@mail.ru>
2024-05-12 15:41:27 +0500

    Init commit

---
+++ users/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'users/home.html')
+
+
+class SignUp(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
